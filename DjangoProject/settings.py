3c79ae5f94d30a7e0bb78572ce8b8679fcf7613b
330-shh <1405874651@qq.com>
2025-07-12 23:48:01 +0800

    final

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts',
     'learning_log',
+    'django_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
